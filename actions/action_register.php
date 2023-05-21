edc98be52ad9131c86b084cb7a3091d0e9671b36
jcribeiro <j.alberto.cribeiro@gmail.com>
2023-05-21 20:40:07 +0100

    Fixed user register

--- actions/action_register.php
+++ actions/action_register.php
@@ -12,28 +12,44 @@
     $bio = $_POST['bio'];
 
     $db = getDatabaseConnection();
+    var_dump($_POST);
+    var_dump($_FILES);
 
     $user = User::getUser($db, $username);
-
+    var_dump($user);
 
     try {
-        if ($user) {
+        if (!is_null($user)) {
             $session->addMessage('error', 'username already exists!');
-            header('Location: /register.php' );
+            echo "1 ";
+            var_dump($_SESSION['messages']);
+            //header('Location: /register.php' );
         } else {
-            if((strlen($password)>=1 and strlen($password)<=7) or (strlen($confirm)>=1 and strlen($confirm)<=7)) {
+            if((strlen($password) >= 1 and strlen($password) <= 7) or (strlen($confirm) >= 1 and strlen($confirm) <= 7)) {
                 $session->addMessage('error', 'password is too short');
-                header('Location: /register.php' );
-            } else if(strlen($password)>7 or strlen($confirm)>7){
-                if ($password != $confirm) {
-                $session->addMessage('error', 'passwords do not match');
-                header('Location: /register.php' );
+                echo "2 ";
+                var_dump($_SESSION['messages']);
+                //header('Location: /register.php' );
+            } elseif (strlen($password) > 7 or strlen($confirm) > 7) {
+                if ($password !== $confirm) {
+                    $session->addMessage('error', 'passwords do not match');
+                    echo "3 ";
+                    var_dump($_SESSION['messages']);
+                    //header('Location: /register.php' );
+                } else {
+                    try { 
+                        User::register($db, $username, $email, $password, $bio);
+                        $session->addMessage('success', 'Successfully registered!');
+                        echo "4 ";
+                        var_dump($_SESSION['messages']);
+                        //header('Location: /ticket_list.php' );
+                    } catch (PDOException $e) {
+                        echo 'Database Error: ' . $e->getMessage();
+                    } catch (Exception $e) {
+                        echo 'Error: ' . $e->getMessage();
+                    }
+                }
             }
-            } else {
-                User::register($db, $username, $email, $password, $bio);
-                $session->addMessage('success', 'Successfully registered!');
-                header('Location: /ticket_list.php' );
-            }     
         }
     } catch (PDOException $e) {
         echo 'Database Error: ' . $e->getMessage();
@@ -41,5 +57,7 @@
         echo 'Error: ' . $e->getMessage();
     }
 
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    var_dump($_SESSION['messages']);
+    echo "final";
+    //header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
